dd97c529d261b9a1f2f512bb6258bea48b5b369e
anudharmik <24dharmika@rbunagpur.in>
2026-06-16 22:30:32 +0530

    chore: support dynamic production port

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,8 @@ const server = http.createServer(app);
 
 initSocket(server);
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = Number(process.env.PORT) || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
